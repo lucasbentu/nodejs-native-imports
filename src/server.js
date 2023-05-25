27d7e068c24209f99bff04e2dba1ec2b07713844
lucasbentu <lucas.bento38@gmail.com>
2023-05-24 22:30:46 -0300

    feat():add filter by queryParams with regex

--- src/server.js
+++ src/server.js
@@ -1,26 +1,31 @@
-import http from 'node:http'
-import { json } from './middlewares/json.js'
-import { routes } from './routes.js'
-
+import http from 'node:http';
+import { json } from './middlewares/json.js';
+import { routes } from './routes.js';
+import { extractQueryParams } from './utils/extract-query-params.js';
 
 const server = http.createServer(async (req, res) => {
-  const { method, url } = req
+  const {method, url} = req;
 
-  await json(req, res)
+  console.log(method, url);
+
+  await json(req, res);
 
   const route = routes.find(route => {
-    const routeParams = req.url.match(route.path)
-
-    req.params = { ...routeParams.groups }
-
     return route.method === method && route.path.test(url)
-  })
+  });
   
   if (route) {
-    return route.handler(req, res)
+    const routeParams = req.url.match(route.path);
+
+    const { query, ...params } = routeParams.groups;
+
+    req.params = params;
+    req.query = query ? extractQueryParams(query) : {};
+
+    return route.handler(req, res);
   }
 
-  return res.writeHead(404).end()
+  return res.writeHead(404).end();
 })
 
-server.listen(3333)
+server.listen(3333);
--- src/utils/build-route-path.js
+++ src/utils/build-route-path.js
@@ -1,10 +1,9 @@
-// /users/:id
-
 export function buildRoutePath(path) {
-  const routeParametersRegex = /:([a-zA-Z]+)/g
-  const pathWithParams = path.replaceAll(routeParametersRegex, '(?<$1>[a-z0-9\-_]+)')
+  const routeParametersRegex = /:([a-zA-Z]+)/g;
 
-  const pathRegex = new RegExp(`^${pathWithParams}`)
+  const pathWithParams = path.replaceAll(routeParametersRegex, '(?<$1>[a-z0-9\-_]+)');
 
-  return pathRegex
+  const pathRegex = new RegExp(`^${pathWithParams}(?<query>\\?(.*))?$`);
+
+  return pathRegex;
 }
